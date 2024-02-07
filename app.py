f9b756786baee53baee5c0d882d54f0c4d7e6352
Arham Dev <118837227+Arham1243@users.noreply.github.com>
2024-02-08 01:10:02 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+
+movie_dict = pickle.load(open("pkl-files/movie_dict.pkl","rb"))
+movies = pd.DataFrame(movie_dict)
+similarity= pickle.load(open("pkl-files/similarity.pkl","rb"))
+
+def get_movie_poster(movie_title):
+    api_key = '99f3b2c3'
+    url = f"http://www.omdbapi.com/?t={movie_title}&apikey={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    poster_url = data.get('Poster', '')
+    return poster_url
+
+def recommend(movie_title):
+    movie_index = movies[movies['title'] == movie_title].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for movie in movies_list:
+        movie_name = movies.iloc[movie[0]].title
+        recommended_movies.append(movie_name)
+        poster_url = get_movie_poster(movie_name) or "images/placeholder.jpg"
+        recommended_movies_posters.append(poster_url)
+    return recommended_movies,recommended_movies_posters
+
+
+st.set_page_config(
+    page_title="Movie Recommendation: Find Related Films",
+    page_icon="🎥",
+    layout="wide",
+)
+
+
+
+st.title("Discover Related Movies")
+selected_movie = st.selectbox(
+    "Enter your favorite movie name and click 'Recommend' to discover five related movie suggestions!",
+    movies['title'].values)
+
+if st.button('Recommend'):
+    st.title("Recommended Movies")
+    recommended_movies,posters = recommend(selected_movie)
+
+    col1, col2, col3 , col4 ,col5 = st.columns(5,gap="medium")
+
+    with col1:
+        st.image(posters[0])
+        st.subheader(recommended_movies[0])
+    with col2:
+        st.image(posters[1])
+        st.subheader(recommended_movies[1])
+    with col3:
+        st.image(posters[2])
+        st.subheader(recommended_movies[2])
+    with col4:
+        st.image(posters[3])
+        st.subheader(recommended_movies[3])
+    with col5:
+        st.image(posters[4])
+        st.subheader(recommended_movies[4])
