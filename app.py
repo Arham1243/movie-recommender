c3581ead74833c7220a1750664438083dec2c9c4
Arham Dev <118837227+Arham1243@users.noreply.github.com>
2024-02-07 20:34:04 +0000

    commit

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import pandas as pd
 import requests
 from io import BytesIO
 
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
 movie_dict = pickle.load(open("pkl-files/movie_dict.pkl","rb"))
 movies = pd.DataFrame(movie_dict)
 similarity_url = "https://demo-designprojects.com/demo/test/similarity.pkl"
 
-response = requests.get(similarity_url)
-
+response = requests.get(similarity_url,headers=headers)
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
     # Load the pickle file from the response content
